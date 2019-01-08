9e345b998149a543a40a648cf952fc7b26820807
Todd Pike <todd.pike@llamasoft.com>
2019-01-08 09:44:01 -0500

    ex 4 - complete

--- exercise-4/greetings.js
+++ exercise-4/greetings.js
@@ -1,12 +1,21 @@
 
 export function buildGreeting(name) {
+    return `Hello, ${name}! How are you?`;
 }
 
 export function choosePartingWord(language) {
+    return (language === "Spanish") ? "Adios!" : "Goodbye!";
 }
 
 export function choosePartingWordFromObject(data) {
+    const { name, language } = data;
+    return `Goodbye, ${name}!`;
+}
+
+export function choosePartingWordFromObject2({ name }) {
+    return `Goodbye, ${name}!`;
 }
 
 export function mapPartingWords(languages) {
+    return languages.map(value => choosePartingWord(value));
 }
